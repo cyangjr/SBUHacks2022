d4dbef324d25ba2167f882111296e3aaced99d69
Wilson <71456315+wilsonw13@users.noreply.github.com>
2022-09-24 16:19:13 -0400

    commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import Navbar from "../../components/global/Navbar";
+import Reminder from "../../components/home/Reminder";
+
+export default function index() {
+  const data = [0, 1, 2];
+
+  return (
+    <>
+      <Navbar></Navbar>
+      {data.map((e) => {
+        return <Reminder reminderID={e} />;
+      })}
+    </>
+  );
+}
