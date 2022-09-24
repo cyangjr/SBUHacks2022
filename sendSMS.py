608d18f699f418f2d3400fd4dd52eee56ef5f31b
adriannap <adri.peng@gmail.com>
2022-09-24 19:31:11 -0400

    post req receiver

--- sendSMS.py
+++ sendSMS.py
@@ -1,27 +1,13 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
+import requests
 from twilio.rest import Client
-from flask import Flask
+from flask import Flask, request
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-
-@app.route("/api/send_message")
-def send_Message():
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    auth_token = os.environ['TWILIO_AUTH_TOKEN']
-    client = Client(account_sid, auth_token)
-
-    message = client.messages.create(
-        body='Hi there',
-        from_='+18596483770',
-        to='+19177699558'
-    )
-
-    return message.sid
-
-# Find your Account SID and Auth Token in Account Info
-# Instructions for setting env variables
+# Instructions for setting Account SID and Auth Token variables
 # MAC & LINUX
 #   echo "export TWILIO_ACCOUNT_SID='<ACCOUNT SID>'" > twilio.env
 #   echo "export TWILIO_AUTH_TOKEN='<AUTH TOKEN'" >> twilio.env
@@ -29,3 +15,39 @@ def send_Message():
 # WINDOWS
 #   set TWILIO_ACCOUNT_SID=ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 #   set TWILIO_AUTH_TOKEN=your_auth_token
+
+# route decorator tells Flask what url should trigger the function below
+
+# use FormData object in fetch request to pass data for sending message
+# get 
+endpoint = "https://localhost:5000/api/send_message"
+load_dotenv(dotenv_path="twilio.env")
+# params = {name : 'Adrianna'}
+
+@app.route("/api/send_message", methods=["POST"]) 
+def send_Message():
+    '''
+    Responds to the post request sent by JS
+    
+    '''
+    if request.method == "POST":
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
+
+        
+        name = request.form["name"]
+        med = request.form["medication"]
+        phone = request.form["phone_number"]
+        res = f"{name} should take {med}"
+
+        message = client.messages.create(
+            body=res,
+            # form data is like dictionary
+            from_='+18596483770',
+            to='+1' + phone
+        )
+    return ""
+    
+
+# process: load env variables, export FLASK_APP=file.py, flask run, curl http://localhost:5000/api/send_message
