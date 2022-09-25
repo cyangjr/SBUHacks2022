97bbecf18b5395ad888a36912c1bd30671b05e2c
Wilson <71456315+wilsonw13@users.noreply.github.com>
2022-09-24 23:54:14 -0400

    helpful commit message

--- components/global/Navbar.jsx
+++ components/global/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import logo from "../../public/imgs/logo.png";
 import pfp from "../../public/imgs/default_pfp.jpg";
 import styles from "../../styles/Navbar.module.css";
 
 export default function navbar() {
   return (
     <div className={styles.container}>
+      <div className={styles.logo_container}>
+        <img src={logo.src} alt="" />
+      </div>
       <div className={styles.title_container}>
         <h1>forget med-not</h1>
       </div>
@@ -12,14 +16,14 @@ export default function navbar() {
       <div className={styles.link_container}>
         <ul>
           <li>
-            <a href="">About</a>
+            <a href="#">About</a>
           </li>
           <li>
-            <a href="">Contact</a>
+            <a href="#">Contact</a>
           </li>
           <li>
             <div className={styles.profile}>
-              <img src={pfp} alt="Profile Picture" />
+              <img className={styles.pfp} src={pfp.src} alt="Profile Picture" />
             </div>
           </li>
         </ul>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,27 @@
-import Head from "next/head";
 import styles from "../../styles/Home.module.css";
+
 import Navbar from "../../components/global/Navbar";
 import Reminder from "../../components/home/Reminder";
+
+import plus_svg from "../../public/svgs/plus-sign.svg";
 
 export default function index() {
   const data = [0, 1, 2];
 
   return (
     <>
-      <Navbar></Navbar>
-      {data.map((e) => {
-        return <Reminder reminderID={e} />;
-      })}
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.add_container}>
+          <div className={styles.svg_container}>
+            <img className={styles.plus_svg} src={plus_svg.src} alt="Add" />
+          </div>
+          <p className={styles.add_note}>Click to add a reminder ...</p>
+        </div>
+        {data.map((e) => {
+          return <Reminder reminderID={e} />;
+        })}
+      </div>
     </>
   );
 }
